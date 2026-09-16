90e902f43863d09fc6451f8c7f50fed7c096bcc3
Aditya Yaduvanshi <aditya97y@gmail.com>
2026-09-17 00:59:12 +0530

    added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['mqgoqkhkbjcxdwiyttdg.supabase.co'],
+    domains: ['mqgoqkhkbjcxdwiyttdg.supabase.co', "pypvwpfoaghmizbogjeo.supabase.co"],
   },
 };
 
